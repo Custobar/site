ed52d6c2084a112939fc6bd781a6015aa4fde3d4
Korobeynik <korobeynik77@gmail.com>
2018-02-22 12:09:49 +0200

    Psd to html, main page/slider

--- themes/custobar/src/js/global.js
+++ themes/custobar/src/js/global.js
@@ -2,4 +2,5 @@ let jQuery = require("jquery");
 
 import "../sass/global.sass";
 import "./modules/libs/jquery.magnific-popup.min.js"
+import "./modules/libs/slick.js"
 import "./modules/common.js"
--- themes/custobar/src/js/modules/common.js
+++ themes/custobar/src/js/modules/common.js
@@ -2,5 +2,15 @@
 $(document).ready( function() {
 
   //$('.popup').magnificPopup();
+  	//Slider
+
+  	$('.slider').slick({
+  		dots: false,
+      autoplay: true,
+      autoplaySpeed: 8000,
+      mobileFirst: true,
+      speed: 500
+  	});
+
 
 });	
