0ef0007f938a83bbb75d8b9fadb2a52b2bd2c5d3
Korobeynik <korobeynik77@gmail.com>
2018-03-02 13:08:55 +0200

    testimonial page - video section done

--- themes/custobar/src/js/modules/common.js
+++ themes/custobar/src/js/modules/common.js
@@ -1,10 +1,18 @@
 
 $(document).ready( function() {
 
+  $('.video-bg').click(function(e) {
+    e.preventDefault();
+    $('#video').html('<iframe src="https://player.vimeo.com/video/149581829?title=0&byline=0&portrait=0;autoplay=true"  frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen ></iframe>');
+    $(this).css('display', 'none');
+  });
+
 	$(".ninja-btn").click( function() {
 	  $(this).toggleClass("active");
 	  $('.menu').slideToggle(400);
 	});
+
+
 
   //$('.popup').magnificPopup();
   	//Slider
